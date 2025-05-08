c2f31d610bf102d5e827f6d74ea6f972b30871a8
Mariane <mariane.amendes@eaportal.org>
2025-05-08 11:37:18 -0300

    função

---
+++ main.js
@@ -0,0 +1,43 @@
+/*EXERCICIO 1
+
+function mensagem() {
+    alert(`Seja muito bem-vindo(a)!`)
+    console.log("Mais uma vez, seja bem vindo(a)")
+}
+
+mensagem ()
+*/
+
+
+
+
+/*EXERCICIO 2
+
+function somar (valor1, valor2) {
+    alert(valor1 + valor2)
+}
+ somar(30, 40)
+*/
+
+
+ /*EXERCICIO 3
+
+ function mensagemPessoal(nome, cargo, anosTrabalhadas) {
+    alert(`Seja bem-vindo(a) ${cargo} ${nome}. Esteamos felizes por ter você trabalhando conosco há ${anosTrabalhados} anos.`)
+ }
+ mensagemPessoal("Lucas", "Gerente", 10)
+*/
+
+
+ /*EXERCICIO 4
+
+ function somarEDevolver(n1, n2, n3) {
+    let resultado = n1 + n2 + n3return resultado
+ }
+
+ let soma1 = somarEDevolver(18, 11, 12)
+ let soma2 = somarEDevolver(30, 40, 50)
+
+ alert(soma1)
+ alert(soma2)
+ */
